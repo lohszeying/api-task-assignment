d913269fb08b41bfffed27d62f7da18841198cc7
Sze Ying <bloom9334@hotmail.com>
2025-10-12 23:58:41 +0800

    chore: remove test gemini endpoint

--- src/routes/healthRoutes.ts
+++ src/routes/healthRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { checkDatabaseHealth, getApiStatus, getGemini } from '../controllers/healthController';
+import { checkDatabaseHealth, getApiStatus } from '../controllers/healthController';
 
 const router = Router();
 
 router.get('/', getApiStatus);
 router.get('/db-health', checkDatabaseHealth);
-router.get('/gemini', getGemini)
 
 export default router;
