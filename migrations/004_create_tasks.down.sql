8996cef3e129c89b60bd67229520cfc92a3751d0
Sze Ying <bloom9334@hotmail.com>
2025-10-15 01:21:06 +0800

    chore: add index to parent_task_id on tasks table

--- migrations/004_create_tasks.down.sql
+++ migrations/004_create_tasks.down.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+DROP INDEX IF EXISTS idx_tasks_parent_task_id;
+
 DROP TABLE IF EXISTS tasks;
 
 COMMIT;
--- migrations/004_create_tasks.up.sql
+++ migrations/004_create_tasks.up.sql
@@ -27,4 +27,6 @@ CREATE TABLE IF NOT EXISTS tasks (
     ON DELETE CASCADE
 );
 
+CREATE INDEX IF NOT EXISTS idx_tasks_parent_task_id ON tasks (parent_task_id);
+
 COMMIT;
